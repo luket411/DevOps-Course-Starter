aab9e8e7bd6edd61a9d97419a689c0a438a14df8
Taylor Luke (ETAS-DEV/XPC-UK)# <Luke.Taylor@etas.com>
2023-01-06 15:05:10 +0000

    Displays updated index.html and functionality added to expand to do list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template, request, redirect
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World!'
+    if request.method == "GET":
+        items = [f"#{item['id']} {item['status']} - {item['title']}" for item in get_items()]
+        return render_template('index.html', items=items)
+    elif request.method == "POST":
+        add_item(request.form.get('title'))
+        return redirect("/")
