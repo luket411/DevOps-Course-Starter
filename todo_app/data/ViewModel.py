4b40b6b3cb9a8606381d98757ec9dc9524f88f73
Taylor Luke (ETAS-DEV/XPC-UK)# <Luke.Taylor@etas.com>
2023-04-06 10:55:49 +0100

    Added functionality to View Model

--- todo_app/data/ViewModel.py
+++ todo_app/data/ViewModel.py
@@ -1,7 +1,8 @@
 class ViewModel():
-    def __init__(self, all_items) -> None:
-        self._open_items = [item for item in all_items if item.status == "To Do"]
+    def __init__(self, all_items=[]) -> None:
+        self._backlog_items = [item for item in all_items if item.status == "To Do"]
         self._completed_items = [item for item in all_items if item.status == "Done"]
+        self._open_items = [item for item in all_items if item.status == "Doing"]
 
     @property
     def open_items(self):
@@ -10,3 +11,11 @@ class ViewModel():
     @property
     def completed_items(self):
         return self._completed_items
+    
+    @property
+    def backlog_items(self):
+        return self._backlog_items
+
+    @property
+    def items(self):
+        return self._backlog_items + self._completed_items + self._open_items
