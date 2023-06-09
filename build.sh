0efe3453cfcb10728e3e26488df3a696f0e32e82
Luke <lnt200011@gmail.com>
2023-06-09 11:42:05 +0100

    Added docker-compose.yml

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ echo "building prod ..."
 docker build --target production --tag todo-app:prod .
 echo "building dev ..."
 docker build --target development --tag todo-app:dev .
+
+# These files are being kept as I haven't set up docker compose to work properly with the office proxy. So it's useful to still have these additionally
