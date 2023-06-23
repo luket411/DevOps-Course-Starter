d7739561a0d418c0cc8cf6b77a286eb9efa728a2
Taylor Luke (ETAS-DEV/XPC-UK)# <Luke.Taylor@etas.com>
2023-06-23 10:51:28 +0100

    Fixed style errors of e2e test

--- e2e_tests/test_e2e.py
+++ e2e_tests/test_e2e.py
@@ -4,10 +4,11 @@ from time import sleep
 from threading import Thread
 from todo_app import app
 from dotenv import find_dotenv, load_dotenv
-from requests  import post, delete, get
+from requests import post, delete, get
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 
 def create_trello_board():
     url = "https://api.trello.com/1/boards/"
@@ -37,7 +38,7 @@ def delete_trello_board(board_id):
 
     response = delete(
         url,
-        params=query, 
+        params=query,
         verify=False
     )
 
@@ -48,7 +49,7 @@ def delete_trello_board(board_id):
 def app_with_temp_board():
     file_path = find_dotenv('.env')
     load_dotenv(file_path, override=True)
-    
+
     # Create the new board & update the board id environment variable
     board_id = create_trello_board()
     os.environ['TRELLO_BOARD_ID'] = board_id
@@ -69,15 +70,15 @@ def app_with_temp_board():
     thread.join(1)
     delete_trello_board(board_id)
 
+
 @pytest.fixture(scope="module")
 def driver():
-    
+
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
-    
-    
+
     with webdriver.Chrome(options=chrome_options) as driver:
         yield driver
 
@@ -86,7 +87,8 @@ def test_app_is_running(app_with_temp_board):
     response = get(url="http://localhost:5000")
     assert response.status_code == 200
 
+
 def test_task_journey(driver, app_with_temp_board):
     driver.get('http://localhost:5000')
-    
+
     assert driver.title == 'To-Do App'
